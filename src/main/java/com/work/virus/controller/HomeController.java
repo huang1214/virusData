759791c20544c6edbf4a2dd3711a6262b29a47d1
hwj <1318935770@qq.com>
2020-02-28 20:41:22 +0800

    导入了layui文件，写了一部分主页内容以及controller

---
+++ src/main/java/com/work/virus/controller/HomeController.java
@@ -0,0 +1,29 @@
+package com.work.virus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+
+/**
+ *  负责页面的跳转，登录注册等行为，在主页访问页面等
+ */
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+    @RequestMapping("/login")
+    public String login(HttpServletRequest request, HttpServletResponse response){
+        // 清空session
+        Enumeration em = request.getSession().getAttributeNames();
+        while(em.hasMoreElements()){
+            request.getSession().removeAttribute(em.nextElement().toString());
+        }
+        return "login";
+    }
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,10 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+<%--调至登录页--%>
+<%--<jsp:forward page="/home/login"></jsp:forward>--%>
+
+<%--跳至主页--%>
+<jsp:forward page="/home/index"></jsp:forward>
 </body>
 </html>
