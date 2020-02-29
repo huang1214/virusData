9ff9b690d0444437a9e2425839f16bf8e1863d74
nehssss <52525885+nehssss@users.noreply.github.com>
2020-03-01 05:23:15 +0800

    疫情新闻接口

--- src/main/java/com/work/virus/dao/NewsMapper.java
+++ src/main/java/com/work/virus/dao/NewsMapper.java
@@ -1,6 +1,8 @@
 package com.work.virus.dao;
 
 import com.work.virus.pojo.News;
+import org.apache.ibatis.annotations.Param;
+
 import java.util.List;
 
 public interface NewsMapper {
@@ -8,9 +10,15 @@ public interface NewsMapper {
 
     int insert(News record);
 
+    int rep(News record);
+
     News selectByPrimaryKey(String id);
 
     List<News> selectAll();
 
     int updateByPrimaryKey(News record);
+
+    List<News> selectNews(@Param("title") String title);
+
+    int update(@Param("id") String id, @Param("field") String field, @Param("value") String value);
 }
